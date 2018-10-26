58a48355061749b7369f61fe11d6301b40956989
Eric Santos <ericsantosbr2@gmail.com>
2018-10-25 23:13:18 -0300

    old player object removed

--- player.lua
+++ player.lua
@@ -29,44 +29,6 @@ function player.new(world, x, y, width, height)
 		love.graphics.polygon("fill", self.body:getWorldPoints(self.shape:getPoints()))
 	end
 
-	player1.update = function(self, keys)
-		if keys.up == true then
-			if player1.y > 0 then
-				player1.y = player1.y - 4
-			else
-				player1.y = 0
-			end
-		end
-		
-		if keys.down == true then
-			if player1. y < (love.graphics.getHeight() - player1.height) then
-				player1.y = player1.y + 4
-			else
-				player1.y = love.graphics.getHeight() - player1.height
-			end
-		end
-
-		if keys.left == true then
-			if player1.x > 0 then
-				player1.x = player1.x - 4
-			else
-				player1.x = 0
-			end
-		end
-
-		if keys.right == true then
-			if player1.x + player1.width < love.graphics.getWidth() then
-				player1.x = player1.x + 4
-			else
-				player1.x = love.graphics.getWidth() - player1.width
-			end
-		end
-	end
-
-	player1.draw = function(self)
-		-- love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
-	end
-
 	return newPlayer
 end
 
